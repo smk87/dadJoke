095bf91b7ebbf0210170b540bc68767b8371cb94
Saqlain Mahmud <30999133+smk87@users.noreply.github.com>
2019-09-17 10:36:30 +0600

    Joke comp added.

--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -1,11 +1,56 @@
 <template>
   <div>
-    jokes
+    <Joke
+      v-for="joke in jokes"
+      :id="joke.id"
+      :key="joke.id"
+      :joke="joke.joke"
+    />
   </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios';
+import Joke from '../../components/Joke';
+
+export default {
+  components: {
+    Joke
+  },
+  data () {
+    return {
+      jokes: []
+    };
+  },
+  async created () {
+    const config = {
+      headers: {
+        Accept: 'application/json'
+      }
+    };
+
+    try {
+      const res = await axios.get('https://icanhazdadjoke.com/search', config);
+
+      this.jokes = res.data.results;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  head () {
+    return {
+      title: 'Dad Jokes',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for corny dad jokes'
+        },
+        {}
+      ]
+    };
+  }
+};
 </script>
 
 <style>
